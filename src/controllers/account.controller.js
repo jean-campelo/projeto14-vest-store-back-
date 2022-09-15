cd5b071e4ad9e519438efe53dcc9540a7bf55e33
Jean Campelo <jean93campelo@gmail.com>
2022-09-15 19:27:23 -0300

    Recebendo dados e validando campos

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,7 +1,24 @@
 import db from "../database/db.js";
 
+const validationNewUser = joi.object({
+  name: joi.required().string().empty(" "),
+  email: joi.required().email(),
+  password: joi.required().strinf(),
+});
+
 async function registerNewUser(req, res) {
-  
-};
+  const { name, email, passord, imageProfile } = req.body;
+
+  const validationNewUser = newUserSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationNewUser.error) {
+    const errors = validationNewUser.error.details.map(
+      (detail) => detail.message
+    );
+    return res.status(422).send({ message: errors });
+  }
+}
 
 export { registerNewUser };
